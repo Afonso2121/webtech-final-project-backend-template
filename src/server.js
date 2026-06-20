0914c5c558d9f340a1b92ef6bd1ffb76cb4c969d
EricQSC <anacmmanulc@gmail.com>
2026-06-20 18:34:18 +0100

    configuração da base de dados e modelo de utilizador

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db"); // <-- 1. Adiciona isto
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger docs:  http://localhost:${PORT}/api-docs`);
+// 2. Chama a função antes de ligar o servidor
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Swagger docs:  http://localhost:${PORT}/api-docs`);
+  });
 });
